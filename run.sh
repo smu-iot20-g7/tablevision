31556868f6b864097ab57087a7d3d5696835b83a
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2020-10-11 22:12:16 +0800

    Updated: run script

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 export CLARIFAI_SECRET=
+export TELEGRAM_KEY=
 export PI_IPV4=192.168.1.74
+export TABLEVISION_API=http://localhost:5000/tables
+#export TABLEVISION_API=https://ui2lc3zrbc.execute-api.ap-southeast-1.amazonaws.com/dev/tables
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 alias python=python3
 python "$DIR/tablevision.py"
