6099688184fe4c35565bf1ccf53324cc3f26cea8
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2020-10-11 22:13:30 +0800

    Implemented: Occupancy state

--- tablevision.py
+++ tablevision.py
@@ -1,9 +1,14 @@
-import sys, time, os, base64
+import sys, time, os, base64, requests
 from datetime import datetime
 import cv2
 from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
 from clarifai_grpc.grpc.api.status import status_pb2, status_code_pb2
+
+TELEGRAM_KEY = os.environ["TELEGRAM_KEY"]
+TELEGRAM_URL = "https://api.telegram.org/bot" + TELEGRAM_KEY
+
+TABLEVISION_API = os.environ["TABLEVISION_API"]
 
 # ======================================
 # Clarifai Setup
@@ -31,7 +36,18 @@ except:
 # video frames can be read
 vid_capture, frame = video_capture.read()
 
-while(vid_capture):
+def hasPeople(people_predictor):
+    if people_predictor["no person"] >= people_predictor["people"]:
+        return False
+    else:
+        return True
+
+def hasCrockeries(crockeries_predictor):
+    if len(crockeries_predictor) != 0:
+        return True
+    return False
+
+while (vid_capture):
     # Capture frame-by-frame
     vid_capture, frame = video_capture.read()
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -68,21 +84,47 @@ while(vid_capture):
 
     # Since we have one input, one output will exist here.
     output = post_model_outputs_response.outputs[0]
+    prediction_results = output.data.concepts
 
-    # print("===================")
-    # print("Predicted concepts:")
-    # print("===================")
-    for concept in output.data.concepts:
+    people_predictor = {"people": 0.00, "no person": 0.00}
+    crockeries_predictor = {}
 
-        if concept.name == "people" and concept.value > 0.95:
-            print("%s %.2f" % (concept.name, concept.value)) 
+    for concept in prediction_results:
+        if concept.name == "no person" or concept.name == "people":
             print("===================")
-            print("PEOPLE DETECTED")
+            print("%s %.2f" % (concept.name, concept.value))
             print("===================")
-            print("")
 
-    # print("===================")
-    print("")
+            people_predictor[concept.name] = concept.value
+
+        if (concept.name == "food" or concept.name == "tableware" or concept.name == "cutlery") and concept.value > 0.85:
+            crockeries_predictor[concept.name] = concept.value
+
+    
+    table_has_people = hasPeople(people_predictor)
+    table_has_crockeries = hasCrockeries(crockeries_predictor)
+
+    try:
+        if table_has_people:
+            try:
+                msg = "PEOPLE DETECTED, confidence:" + str(concept.value)
+                tablevision_api_response = requests.put(TABLEVISION_API + "/15?state=2")
+                response = requests.get(TELEGRAM_URL + msg)
+                print("Telegram response:" + str(response))
+            except Exception as e:
+                raise
+        elif table_has_crockeries and not table_has_people:
+            try:
+                tablevision_api_response = requests.put(TABLEVISION_API + "/15?state=1")
+            except Exception as e:
+                raise
+        else:
+            try:
+                tablevision_api_response = requests.put(TABLEVISION_API + "/15?state=0")
+            except Exception as e:
+                raise
+    except Exception as e:
+        print("can't send to api, " + str(e))
 
 # When everything done, release the capture
 video_capture.release()
