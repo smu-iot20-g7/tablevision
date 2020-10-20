a599913002ac9f99d5f37c9b57c53d2cf5b5373f
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2020-10-20 21:30:37 +0800

    Created: Tablevision v2 test

--- test.py
+++ test.py
@@ -1,13 +1,51 @@
-from table import Table
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+#import cv2
+import json
+import base64
+import requests
 
-table15 = Table(15)
-# A to C
-table15.did_change_state(2)
-print(table15.print_states())
-# C to C
-print("Same state again")
-table15.did_change_state(0)
-print(table15.print_states())
-# B to A
-table15.did_change_state(0)
-print(table15.print_states())
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+
+# allow the camera to warmup
+time.sleep(0.1)
+
+# grab an image from the camera
+#camera.capture(rawCapture, format="bgr")
+#image = rawCapture.array
+#while True:
+camera.capture("test.jpg")
+image = "test.jpg"
+image_64 = base64.encodestring(open(image, "rb").read())
+print(image_64)
+
+r = {
+  "requests": [
+    {
+      "image": {
+        "content": image_64
+      },
+      "features": [
+        {
+          "maxResults": 10,
+          "type": "OBJECT_LOCALIZATION"
+        },
+      ]
+    }
+  ]
+}
+
+json_request = json.dumps(r)
+
+with open("key.json", "r") as key:
+    print()
+    gcp_key = "Bearer " + key.read().strip()
+
+headers = {"Authorization": gcp_key, "Content-Type": "application/json; charset=utf-8"}
+response = requests.post("https://vision.googleapis.com/v1/images:annotate", headers=headers, data=json_request)
+
+print(response.text)
